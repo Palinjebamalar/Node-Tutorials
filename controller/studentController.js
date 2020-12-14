87d191cc45f9825b1cbe63ab44dc6e472298aa14
Palin <palinpalin7@gmail.com>
2020-12-14 14:03:02 +0530

    Simple Create

--- controller/studentController.js
+++ controller/studentController.js
@@ -1,104 +1,14 @@
 const Student=require('../model/student')
 
 exports.create = (req, res) => {
-    // Validate request
-    if(!req.body.name) {
-        return res.status(400).send({
-            message: "Name Required"
-        });
-    }
-
-    // Create a Note
-
     Student.create({name:req.body.name,class:req.body.class})
 
     .then(data => {
+        console.log(data)
         res.send(data);
     }).catch(err => {
         res.status(500).send({
-            message: err.message || "Some error occurred while creating the Student."
-        });
-    });
-};
-
-exports.findAll = (req, res) => {
-    Student.find()
-    .then(student => {
-        res.send(student);
-    }).catch(err => {
-        res.status(500).send({
-            message: err.message || "Some error occurred while retrieving Students."
-        });
-    });
-};
-
-exports.findOne = (req, res) => {
-    Student.findById(req.params.id)
-    .then(student => {
-        if(!student) {
-            return res.status(404).send({
-                message: "student not found with id " + req.params.id
-            });            
-        }
-        res.send(student);
-    }).catch(err => {
-        if(err.kind === 'ObjectId') {
-            return res.status(404).send({
-                message: "student not found with id " + req.params.id
-            });                
-        }
-        return res.status(500).send({
-            message: "Error retrieving note with id " + req.params.id
-        });
-    });
-};
-
-exports.update = (req, res) => {
-    // Validate Request
-    if(!req.body.name) {
-        return res.status(400).send({
-            message: "Name Required"
-        });
-    }
-
-    // Find note and update it with the request body
-    Student.updateOne({_id:req.params.id}, { name:req.body.name,class:req.body.class})
-    .then(student => {
-        if(!student) {
-            return res.status(404).send({
-                message: "Student not found with id " + req.params.id
-            });
-        }
-        res.send(student);
-    }).catch(err => {
-        if(err.kind === 'ObjectId') {
-            return res.status(404).send({
-                message: "Student not found with id " + req.params.id
-            });                
-        }
-        return res.status(500).send({
-            message: "Error updating Student with id " + req.params.id
-        });
-    });
-};
-
-exports.delete = (req, res) => {
-    Student.findByIdAndRemove(req.params.id)
-    .then(student => {
-        if(!student) {
-            return res.status(404).send({
-                message: "Student not found with id " + req.params.id
-            });
-        }
-        res.send({message: "Student deleted successfully!"});
-    }).catch(err => {
-        if(err.kind === 'ObjectId' || err.name === 'NotFound') {
-            return res.status(404).send({
-                message: "Student not found with id " + req.params.id
-            });                
-        }
-        return res.status(500).send({
-            message: "Could not delete student with id " + req.params.id
+            message:"Some error occurred while creating the Student."
         });
     });
 };
--- route/route.js
+++ route/route.js
@@ -1,21 +1,9 @@
 module.exports = (app) => {
-    const passport = require('passport')
 
     const Student = require('../controller/studentController');
-    const User = require('../controller/userController');
 
-
-    const needsAuth = passport.authenticate('jwt', { session: false })
-    require('./../middleware/passport')(passport)
 
     // Create a new Note
     app.post('/create', Student.create);
-    app.get('/get', Student.findAll);
-    app.get('/getById/:id', Student.findOne);
-    app.put('/update/:id', Student.update);
-    app.delete('/delete/:id', Student.delete);
 
-    app.post('/register',User.register)
-    app.post('/login',User.login)
-    app.get('/getUSer',needsAuth,User.getUser)
 }
